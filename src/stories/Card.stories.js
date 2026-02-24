222bc5a38664d0207154cae9ad326f13831f1a0e
Plush777 <sky11916@naver.com>
2026-02-24 16:25:11 +0900

    .

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -1,5 +1,28 @@
-import ButtonGroup from '@/components/styleguide/contents/ButtonGroup.astro';
+import MyPostList from '@/components/MyPostList.astro';
+import Logo from '@/components/Logo.astro';
 
 export default {
-	component: ButtonGroup
+	component: MyPostList
+};
+
+export const Post = {
+	args: {
+		withBehavior: false,
+		data: [
+			{
+				id: 1,
+				data: {
+					title: '✍️ 이프 홈페이지 개편 프로젝트 회고록',
+					description: '사내 홈페이지 개편 프로젝트를 진행하며 얻은 것들...',
+					author: '서규영',
+					pubDate: '2026-02-23T00:00:00.000Z',
+					heroImage: '',
+					category: '회고',
+					isWarning: false
+				}
+			}
+		],
+		startIndex: 0,
+		pageSize: 8
+	}
 };
